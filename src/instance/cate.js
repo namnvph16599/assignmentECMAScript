a02be6cc4848b007ac9e5737c6198cf87ed3ce46
Ngô Vưn Nam <anh02284@gmail.com>
2022-04-12 15:39:15 +0700

    api categiry

--- src/instance/cate.js
+++ src/instance/cate.js
@@ -1,24 +1,24 @@
 import instance from "./instance";
 
 export const getAllCates = () => {
-  const url = "/categories";
+  const url = "/category";
   return instance.get(url);
 };
 
 export const getCate = (id) => {
-  const url = `/categories/${id}`;
+  const url = `/category/${id}`;
   return instance.get(url);
 };
 
 export const addCate = (post) => {
-  const url = "/categories";
+  const url = "/category";
   return instance.post(url, post);
 };
 export const updateCate = (post) => {
-  const url = `/categories/${post.id}`;
+  const url = `/category/${post.id}`;
   return instance.put(url, post);
 };
 export const removeCategory = (id) => {
-  const url = `/categories/${id}`;
+  const url = `/category/${id}`;
   return instance.delete(url, id);
 };
